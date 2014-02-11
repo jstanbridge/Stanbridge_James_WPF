1175f6f1fb14974a2cd0c198ff67dd6bf77897bb
jstanbridge <jstanbridge@fullsail.edu>
2014-02-11 13:32:24 -0500

    None

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,1 +1,9 @@
-alert("This is in the js file");
+// Variables of Different Types, Numbers, Booleans and Strings
+
+var age = 38; //Number variable
+
+var isStudent = true; //Boolean Variable
+
+var firstName = "James";
+
+var phrase = 'I don\'t know \n You\'ll have to speak with the professor';
