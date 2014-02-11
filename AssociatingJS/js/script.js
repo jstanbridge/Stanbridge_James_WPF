d50f5568567feb66ee0429dd5124aa1b8ca32f64
jstanbridge <jstanbridge@fullsail.edu>
2014-02-11 13:58:58 -0500

    a+ update

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,8 +1,5 @@
-// Arrays
+//Expressions
 
-var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"]; //0,1,2,3
-
-var num = 3;
-
-muppetNames[num] = "Beaker";
-console.log(muppetNames);
+var a = 2;
+var sum = a + 3;
+console.log(sum);
