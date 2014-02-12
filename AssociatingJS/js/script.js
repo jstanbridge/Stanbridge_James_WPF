6bd2933800cefd6eb3123f34352414d7fb7f3f6c
jstanbridge <jstanbridge@fullsail.edu>
2014-02-12 05:12:47 -0500

    Assignment Operators push.

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,12 +1,5 @@
-//Casting
+//Expressions - Assignment Operators
 
-var stringVar = "6";
-var result = 7 + Number(stringVar);
-console.log(result);
-
-var areaCode = 407;
-var firstPart = 203;
-var secPart = 4529;
-
-var phoneNo = "(" + String(areaCode) + ") " + String(firstPart) + " - " +String(secPart);
-console.log(phoneNo);
+var a = 3;
+a ++ ;
+console.log(a);
