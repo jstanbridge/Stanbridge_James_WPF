7008377b3d64b27d1154eff17a71c89c95849947
jstanbridge <jstanbridge@fullsail.edu>
2014-02-12 12:17:42 -0500

    Prompt and Alert push.

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,5 +1,12 @@
-//Expressions - Assignment Operators
+//Prompt and Alert
 
-var a = 3;
-a ++ ;
-console.log(a);
+//var userInput = prompt("Enter your year of birth:");
+//console.log(userInput);
+
+var width = prompt("We are calculating the area of a rectangle. Please enter the width.");
+var height = prompt("Please enter the height.")
+
+var area = width * height;
+console.log(area);
+
+alert("The area of your rectangle is " + area);
