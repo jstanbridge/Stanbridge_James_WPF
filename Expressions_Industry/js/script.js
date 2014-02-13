1ab1ac96a02543df832b539c48710bb83e946365
jstanbridge <jstanbridge@fullsail.edu>
2014-02-13 12:30:08 -0500

    Declared and defined the variable "websiteWidth" in Expressions_Industry.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -6,4 +6,5 @@
 
 alert("Hello, we are going to calculate the width of the center column of your web page. You can use this to quickly determine changes that should be made if you decide to change the width of one of your outer columns.");
 
+var websiteWidth = prompt("What is the total width (in pixels) of you website?");
 var lColWid = prompt("What is the width (in pixels) of your left-hand column?");
