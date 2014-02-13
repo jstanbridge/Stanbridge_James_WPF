62b3f36c5f4cf90d4b5df9fbc47ac27c1810212c
jstanbridge <jstanbridge@fullsail.edu>
2014-02-13 12:13:07 -0500

    Updated script header for Expressions_Industry.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,12 +1,3 @@
-//Prompt and Alert
-
-//var userInput = prompt("Enter your year of birth:");
-//console.log(userInput);
-
-var width = prompt("We are calculating the area of a rectangle. Please enter the width.");
-var height = prompt("Please enter the height.")
-
-var area = width * height;
-console.log(area);
-
-alert("The area of your rectangle is " + area);
+//James Stanbridge
+//13 Feb 2014
+//Expressions (Industry)
