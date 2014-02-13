6c75b9f509d1158ba58a541186350c7b739553dc
jstanbridge <jstanbridge@fullsail.edu>
2014-02-13 10:01:50 -0500

    Added notes about equation variable in Expressions_Personal.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -11,7 +11,7 @@ alert("Hello! Let's estimate how much you're going to spend on gas for your vehi
 var vehDesc = prompt("Please enter the make and model of your vehicle.");
 //Here we are asking for the vehicle description and storing it as the vehDesc variable to use in our final statement.
 
-
+//Now we collect the actual information we need for the calculation.
 var milesPerWeek = prompt("About how many miles do you travel in one week?");
 
 var mpg = prompt("On average, how many miles does your vehicle get per gallon of gas?");
