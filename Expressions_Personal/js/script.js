58919e0e6d037d78fda508f564b080c5f3a68093
jstanbridge <jstanbridge@fullsail.edu>
2014-02-13 09:48:59 -0500

    Added title and notes for Expressions_Personal.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,3 +1,7 @@
 //James Stanbridge
 //13 Feb 2014
 //Expression (Personal)
+
+//Calculating Gas Expenditures Per Year
+
+//Note: I set this up as an experience for a user, but I can just as easily use it myself and the subject matter is relevant to my life.
