7fda0137f0b0da81b889d10a7182494c0bdd1a9b
jstanbridge <jstanbridge@fullsail.edu>
2014-02-13 09:51:33 -0500

    Defined/declared milesPerWeek variable in Expressions_Personal.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -7,4 +7,9 @@
 //Note: I set this up as an experience for a user, but I can just as easily use it myself and the subject matter is relevant to my life.
 
 alert("Hello! Let's estimate how much you're going to spend on gas for your vehicle this year. Click Ok to get started.");
+//This is a simple alert letting the user know what we are going to calculate.
 var vehDesc = prompt("Please enter the make and model of your vehicle.");
+//Here we are asking for the vehicle description and storing it as the vehDesc variable to use in our final statement.
+
+//Now we collect the actual information we need for the calculation.
+var milesPerWeek = prompt("About how many miles do you travel in one week?");
