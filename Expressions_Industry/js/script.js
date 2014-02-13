6d00bab4e311db5855b7c1d8f086743e3d89e366
jstanbridge <jstanbridge@fullsail.edu>
2014-02-13 12:26:47 -0500

    Added opening alert to Expressions_Industry.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -3,3 +3,5 @@
 //Expressions (Industry)
 
 //This script will calculate the width of a center column in a 3 column layout for a website.
+
+alert("Hello, we are going to calculate the width of the center column of your web page. You can use this to quickly determine changes that should be made if you decide to change the width of one of your outer columns.");
