7bfc90377264c2ade3dda0c252a6ee51c404b89a
jstanbridge <jstanbridge@fullsail.edu>
2014-02-13 16:12:49 -0500

    Added comment to third alert and added fourth alert to Expressions_Wacky.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -5,5 +5,7 @@
 alert("Greetings Captain, I am Commander El Tigre.");
 //This alert begins the dialogue for our story/script.
 alert("As you know, we are facing especially dark days. The hordes of men lie upon our borders and we are hard pressed to defend ourselves.");
-//More alerts to further push the story along.
+//Another alert to further push the story along.
 alert("But never before have all the species of animals united as we have now. We, the proud Royal Army of Zooria, shall not not let this nation fall.");
+//Another alert to further push the story along.
+alert("We have turned to our greatest scientist, Dr. Elefaunt, to create a new weapon to help us face the threat of man. I have summoned you here today to give you the task of assembling the materials needed to craft such a weapon.");
