85b5826072676de4e21abc61033c3b0eb0cd771d
jstanbridge <jstanbridge@fullsail.edu>
2014-02-13 09:49:39 -0500

    Added intro alert to Expressions_Personal.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -5,3 +5,5 @@
 //Calculating Gas Expenditures Per Year
 
 //Note: I set this up as an experience for a user, but I can just as easily use it myself and the subject matter is relevant to my life.
+
+alert("Hello! Let's estimate how much you're going to spend on gas for your vehicle this year. Click Ok to get started.");
