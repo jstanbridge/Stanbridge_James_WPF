93502840fe29dbc794ef931205b0a81385b887e9
jstanbridge <jstanbridge@fullsail.edu>
2014-02-13 12:55:24 -0500

    Added comment to first alert in Expressions_Industry.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -5,6 +5,7 @@
 //This script will calculate the width of a center column in a 3 column layout for a website.
 
 alert("Hello, we are going to calculate the width of the center column of your web page. You can use this to quickly determine changes that should be made if you decide to change the width of one of your outer columns.");
+//Here we are setting up the initial alert to inform the user of what we are calculating.
 
 var websiteWidth = prompt("What is the total width (in pixels) of your website?");
 var lColWid = prompt("What is the width (in pixels) of your left-hand column?");
