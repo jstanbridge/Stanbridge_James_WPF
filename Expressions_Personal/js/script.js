8eac0b25bac02b1406d418276b0ba23054e409ee
jstanbridge <jstanbridge@fullsail.edu>
2014-02-13 09:50:33 -0500

    Defined/Declared first variable.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -7,3 +7,4 @@
 //Note: I set this up as an experience for a user, but I can just as easily use it myself and the subject matter is relevant to my life.
 
 alert("Hello! Let's estimate how much you're going to spend on gas for your vehicle this year. Click Ok to get started.");
+var vehDesc = prompt("Please enter the make and model of your vehicle.");
