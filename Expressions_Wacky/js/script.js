e2e1eeac48f1276dfc4c268fef2c9e299c87dd99
jstanbridge <jstanbridge@fullsail.edu>
2014-02-13 16:20:11 -0500

    Added comment to fifth alert and added aniOne variable to Expressions_Wacky (EW).

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -6,8 +6,11 @@ alert("Greetings Captain, I am Commander El Tigre.");
 //This alert begins the dialogue for our story/script.
 alert("As you know, we are facing especially dark days. The hordes of men lie upon our borders and we are hard pressed to defend ourselves.");
 //Another alert to further push the story along.
-alert("But never before have all the species of animals united as we have now. We, the proud Royal Army of Zooria, shall not not let this nation fall.");
+alert("But never before have all the species of animals united as we have now. We, the proud Royal Army of Zooria, shall not let this nation fall.");
 //Another alert to further push the story along.
 alert("We have turned to our greatest scientist, Dr. Elefaunt, to create a new weapon to help us face the threat of man. I have summoned you here today to give you the task of assembling the materials needed to craft such a weapon.");
 //Another alert to further push the story along.
 alert("You will deliver instructions to four platoons. Three must be selected from these six: Monkeys, Horses, Bears, Rhinos, Vultures, and Bats. The last, you may choose freely.");
+//Another alert to further push the story along.
+
+var aniOne = prompt("Which platoon from the six (Monkeys, Horses, Bears, Rhinos, Vultures, Bats) will you choose first?");
