8212971ab9a584da6411aa74256f3753095c795f
jstanbridge <jstanbridge@fullsail.edu>
2014-02-13 12:47:46 -0500

    Fixed some typos in Expressions_Industry.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -6,7 +6,7 @@
 
 alert("Hello, we are going to calculate the width of the center column of your web page. You can use this to quickly determine changes that should be made if you decide to change the width of one of your outer columns.");
 
-var websiteWidth = prompt("What is the total width (in pixels) of you website?");
+var websiteWidth = prompt("What is the total width (in pixels) of your website?");
 var lColWid = prompt("What is the width (in pixels) of your left-hand column?");
 var rColWid = prompt("What is the width (in pixels) of your right-hand column?");
 
@@ -14,3 +14,4 @@ lColWid = +lColWid;
 rColWid = +rColWid;
 
 lColWid+=rColWid;
+
