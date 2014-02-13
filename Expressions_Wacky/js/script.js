e133d47d17ac983636d743172fe5439c07717cc4
jstanbridge <jstanbridge@fullsail.edu>
2014-02-13 16:09:58 -0500

    Added commentary to first alert and added second alert to Expressions_Wacky.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -3,3 +3,5 @@
 //Expressions (Wacky)
 
 alert("Greetings Captain, I am Commander El Tigre.");
+//This alert begins the dialogue for our story/script.
+alert("As you know, we are facing especially dark days. The hordes of men lie upon our borders and we are hard pressed to defend ourselves.");
