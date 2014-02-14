f688222abfd678f53219d7f5933114eec17395a4
jstanbridge <jstanbridge@fullsail.edu>
2014-02-13 20:54:39 -0500

    Added all comments and finalized Expressions_Wacky.

--- Expressions_Wacky copy/js/script.js
+++ Expressions_Wacky copy/js/script.js
@@ -10,10 +10,10 @@ alert("But never before have all the species of animals united as we have now. W
 //Another alert to further push the story along.
 alert("We have turned to our greatest scientist, Dr. Elefaunt, to create a new weapon to help us face the threat of man. I have summoned you here today to give you the task of assembling the materials needed to craft such a weapon.");
 //Another alert to further push the story along.
-alert("You will deliver instructions to four platoons. Three must be selected from these six: Monkey, Horse, Bear, Rhino, Vulture, and Bat. The last, you may choose freely.");
+alert("You will deliver instructions to four platoons. Three must be selected from these six: Monkeys, Horses, Bears, Rhinos, Vultures, and Bats. The last, you may choose freely.");
 //Another alert to further push the story along.
 
-var aniOne = prompt("Which platoon from the six (Monkey, Horse, Bear, Rhino, Vulture, Bat) will you choose first?");
+var aniOne = prompt("Which platoon from the six (Monkeys, Horses, Bears, Rhinos, Vultures, Bats) will you choose first?");
 //Here we are declaring a variable for animal #1 and defining it via user input. This is really only for flavor and will not factor into our equation.
 var aniTwo = prompt("They do have some strategic advantages. Now, what is your second choice?");
 //Here we are declaring a variable for animal #2 and defining it via user input. This is really only for flavor and will not factor into our equation.
@@ -37,42 +37,34 @@ var fruTwo = prompt("Hmm, " + fruOne + ". And the " + aniTwo + " Platoon? What t
 var fruTwoNum = prompt("How many pieces did they manage to acquire? (Please enter a numerical value, such as 10 or 15)");
 var fruThree = prompt("Alright, moving on. What type of fruit did the " + aniThree + " Platoon collect?");
 var fruThreeNum = prompt("How many pieces did they manage to acquire? (Please enter a numerical value, such as 10 or 15)");
-var fruFour = prompt("I am starting to question if you were the right animal for this task. Let us hope your personal favorites did better. So, what fruit did the " + aniFour + " Platoon collect?");
+var fruFour = prompt("I amm starting to question if you were the right animal for this task. Let uss hope your personal favorites did better. So, what fruit did the " + aniFour + " Platoon collect?");
 var fruFourNum = prompt("How many pieces did they manage to acquire? (Please enter a numerical value, such as 10 or 15)");
 
 alert("Now, the moment of truth.");
-//Another alert to further push the story along.
 alert("Dr. Elefaunt has created a giant fruit cannon. The cannon can fire off 10 pieces of fruit per shot and each shot will destroy 3 platoons of men.");
-//Another alert to further push the story along.
 
-
-//The following four lines ensure that the values for the variables listed below are treat as numerical objects and not strings.
 fruOneNum = +fruOneNum;
 fruTwoNum = +fruTwoNum;
 fruThreeNum = +fruThreeNum;
 fruFourNum = +fruFourNum;
 
 var fruPerShot = 10;
-//Here we declare the variable fruPerShot and set it equal to 10, as given in the text above.
-var allFru=[fruOneNum,fruTwoNum,fruThreeNum,fruFourNum];
-//Here we're placing all the fru#Num variable into an array for easy reference in our equations.
-var fruTotal = +allFru[0] + +allFru[1] + +allFru[2] + +allFru[3];
-//Here we're adding all the values in allFru and assigning it to the variable fruTotal.
-var platDestroyed = (fruTotal%fruPerShot) * 3;
-//Here we calculate how many shots can be fired even given the total amount of fruit collected and how many platoons will be destroyed,  assigning that value to platDestroyed.
-console.log(fruTotal);
-//This cosole command will let us check our math at the end.
+var allFru = array[fruOneNum, fruTwoNum, fruThreeNum, fruFourNum];
+var fruTotal = allfru[1] + allFru[2] + allFru[3] + allFru[4];
 var fruRem = fruTotal%fruPerShot;
-//Here we calculate how many pieces of fruit are left over after all shots have been fired, assigning that to fruRem.
-fruTotal-=fruRem;
-//Now we set fruTotal to equal fruTotal - fruRem for the rest of our calculations.
-var canShots = fruTotal / fruPerShot;
-//This gives us the amount of of time the cannon can be fired by diving the value of fruTotal by fruPerShot and assigning that to the variable canShots
+var canShots = (fruTotal - fruRem) / fruPerShot;
+
+var platDestroyed = (fruTotal / fruPerShot) * 3);
 
 
 
-alert("With the pieces of fruit your platoons acquired we can fire the cannon " + canShots + " times, wiping out " + platDestroyed + " platoons of men with " + fruRem + " pieces of fruit left over. I do not know whether to be proud or disappointed, but we will have to make do with what you have managed to scrounge up. Now, get your platoons back out there and find more fruit! Dismissed!");
-//This is the final alert - a concatenating string that prints the results for the user.
 
-console.log("With the pieces of fruit your platoons acquired we can fire the cannon " + canShots + " times, wiping out " + platDestroyed + " platoons of men with " + fruRem + " pieces of fruit left over. I do not know whether to be proud or disappointed, but we will have to make do with what you have managed to scrounge up. Now, get your platoons back out there and find more fruit! Dismissed!");
-//This is the final console command and is the same as the alert above.
+
+
+
+
+
+
+
+
+
