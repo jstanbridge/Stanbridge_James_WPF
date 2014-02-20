fac81007d58055b514a06efd4bd2b02a3d3805ef
jstanbridge <jstanbridge@fullsail.edu>
2014-02-20 18:50:42 -0500

    Added final notes to Personal.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -7,20 +7,22 @@
 alert("Hi, we're going to help you determine what body area you should workout this week");
 //Here we set the initial alert to let the user know what we are doing.
 
-
-
 var run = prompt("Have you run this week?");
 //Here we set the prompt to find out if the user has run this week.
 if (run === ""){
     alert("You forgot to state whether or not you've run this week!");
     run = prompt("Have you run this week?");
 }
+//The above if statement checks to see if the user failed to input data into the run prompt and returns them to the prompt (once) if they did.
 
 var dayOfWeek = prompt("What day of the week is it?");
+//Here we set the prompt to find out what day it is.
 if (dayOfWeek == ""){
     alert("You forgot to state what day it is!");
     dayOfWeek = prompt("What day of the week is it?");
 }
+//The above if statement checks to see if the user failed to input data into the dayOfWeek prompt and returns them to the prompt (once) if they did.
+
 
 if (run === "No"){
     //This condition is fulfilled is the user states that they have not run this week.
