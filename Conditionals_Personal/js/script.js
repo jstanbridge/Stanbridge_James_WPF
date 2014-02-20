358402541590fb22489a046359145e54512c48d8
jstanbridge <jstanbridge@fullsail.edu>
2014-02-20 18:20:56 -0500

    Added commentary to variables in Personal.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -5,9 +5,12 @@
 //This project will help determine what body area to workout when given the day of the week.
 
 alert("Hi, we're going to help you determine what body area you should workout this week");
+//Here we set the initial alert to let the user know what we are doing.
 
 var run = prompt("Have you run this week?");
+//Here we set the prompt to find out if the user has run this week.
 var dayOfWeek = prompt("What day of the week is it?");
+//Here we set the prompt to determine the day of the week.
 
 if (run === "No"){
     exercise = "cardio";
