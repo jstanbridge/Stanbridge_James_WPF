e542b07f8223e7c8f54afaee491995daa388c4e1
jstanbridge <jstanbridge@fullsail.edu>
2014-02-20 22:19:29 -0500

    Added comments to variable and validation comments in Wacky.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -5,17 +5,22 @@
 //This project will determine if you are a hero or not.
 
 alert("Welcome potential hero! We are going to determine whether or not you qualify for your Hero's License.");
+//This is the initial alert to let the user know what we are focused on.
 
 var maidensSaved = prompt("During your training, how many maidens have you saved?");
+//This variable stores the amount of maidens saved by the user.
 if (maidensSaved === ""){
     prompt("You have to tell us how many maidens you have saved!");
     maidensSaved = prompt("During your training, how many maidens have you saved?");
+    //This is a validation check to make sure that the user has input something into the maidensSaved variable.
 }
 
 var dragonsSlain = prompt("How many dragons have you slain?");
+//This variable stores the amount of dragons slain by the user.
 if (dragonsSlain === ""){
     prompt("I'll ask you again, how many dragons have you slain?");
     dragonsSlain = prompt("During your training, how many maidens have you saved?");
+    //This is a validation check to make sure that the user has input something into the dragonsSlain variable.
 }
 
 if (maidensSaved >= 30 && dragonsSlain >= 15){
