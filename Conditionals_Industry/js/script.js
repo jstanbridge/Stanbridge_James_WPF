d9fdc65eebeb6ee56d834ff8b29842f7d4065272
jstanbridge <jstanbridge@fullsail.edu>
2014-02-20 19:24:11 -0500

    Declared initial variables for Industry.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,3 +1,8 @@
 //James Stanbridge
 //20 Feb 2014
 //Conditionals Industry
+
+//This project will help a user determine whether JPEG of PNG is the best image type to use.
+
+var fileSize;
+var transparency;
