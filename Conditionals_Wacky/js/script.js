db7befd27769318d63bdce5f8afb6339eee74ddf
jstanbridge <jstanbridge@fullsail.edu>
2014-02-20 21:43:16 -0500

    Added validation checks to main variable in Wacky.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -4,5 +4,20 @@
 
 //This project will determine if you are a hero or not.
 
-var maidensSaved;
-var dragonsSlain;
+alert("Welcome potential hero! We are going to determine whether or not you qualify for your Hero's License.");
+
+var maidensSaved = prompt("During your training, how many maidens have you saved?");
+if (maidensSaved === ""){
+    prompt("You have to tell us how many maidens you have saved!");
+    maidensSaved = prompt("During your training, how many maidens have you saved?");
+}
+
+var dragonsSlain = prompt("How many dragons have you slain?");
+if (dragonsSlain === ""){
+    prompt("I'll ask you again, how many dragons have you slain?");
+    dragonsSlain = prompt("During your training, how many maidens have you saved?");
+}
+
+if (maidensSaved >= 30 && dragonsSlain >= 15){
+}
+
