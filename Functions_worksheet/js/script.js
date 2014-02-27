35ba2eefe0c591a33c126ba1af86884bd6848a67
jstanbridge <jstanbridge@fullsail.edu>
2014-02-26 19:29:37 -0500

    Circumference push.

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -2,3 +2,16 @@
 //20 Feb 2014
 //Functions Worksheet
 
+//CIRCUMFERENCE
+
+function circ(r)
+{
+    circCal = 3.14 * r * 2;
+
+    return circCal;
+}
+
+alert("Let's figure out that circumference of a circle!");
+circ(prompt("What is the circle's radius?"));
+
+console.log("The Circumference of the circle is " + circCal);
