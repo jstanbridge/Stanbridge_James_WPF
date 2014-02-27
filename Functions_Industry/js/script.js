53a2274ce6c74ca14ae88915f766ef31fda23f6d
jstanbridge <jstanbridge@fullsail.edu>
2014-02-27 14:27:20 -0500

    Added function call and if else to Industry.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -6,11 +6,15 @@
 
 alert("Let's calculate your wages for this week.");
 
+wages(prompt("How much do you make per hour?"), prompt("How many hours did you work this week?"));
+
 function wages(pay, hrsWrk){
-    if (overtime === true){
+    if (pay === 0 || hrsWrk === 0){
+        alert("You forgot to fill in a field.");
+    }else if (overtime === true){
         var withOT = function (otHrs){
             totalPay = (pay * hrsWrk) + (otHrs * pay * 2);
             return totalPay
-        }
+        }else totalPay = pay * hrsWrk; return totalPay
     }
 }
