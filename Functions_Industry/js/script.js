81decc7d3b02510f60145bb6ea7b75ff32adb476
jstanbridge <jstanbridge@fullsail.edu>
2014-02-27 15:34:58 -0500

    Added overtime prompt to Industry.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -7,12 +7,17 @@
 alert("Let's calculate your wages for this week.");
 
 wages(prompt("How much do you make per hour?"), prompt("How many hours did you work this week?"));
+var overtime = prompt("Did you work overtime this week?");
+
+if (overtime === "Yes" || "yes"){
+    var otHrs = prompt("How many hours of over time did you work?");
+}
 
 function wages(pay, hrsWrk){
     if (pay === 0 || hrsWrk === 0){
         alert("You forgot to fill in a field.");
     }else if (overtime === true){
-        var withOT = function(otHrs){
+        var withOT = function(){
             totalPay = (pay * hrsWrk) + (otHrs * pay * 2);
             return totalPay;
         }
