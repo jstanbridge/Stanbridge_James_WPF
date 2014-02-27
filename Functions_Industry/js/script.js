3be33fff60e14df009fc4f0a849f147adfbb67fe
jstanbridge <jstanbridge@fullsail.edu>
2014-02-27 14:21:38 -0500

    First function and initial if in Industry.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -2,4 +2,15 @@
 //26 Feb 2014
 //Functions Industry
 
-//This project will
+//This project will calculate a week's wage with or without overtime.
+
+alert("Let's calculate your wages for this week.");
+
+function wages(pay, hrsWrk){
+    if (overtime === true){
+        var withOT = function (otHrs){
+            totalPay = (pay * hrsWrk) + (otHrs * pay * 2);
+            return totalPay
+        }
+    }
+}
