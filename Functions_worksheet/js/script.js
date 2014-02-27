9bf430f6c5a70ba663ca1dbc6234cdd34f9091de
jstanbridge <jstanbridge@fullsail.edu>
2014-02-26 20:19:14 -0500

    Stings push.

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -14,4 +14,23 @@ function circ(r)
 alert("Let's figure out that circumference of a circle!");
 circ(prompt("What is the circle's radius?"));
 
+
+alert("The Circumference of the circle is " + circCal);
 console.log("The Circumference of the circle is " + circCal);
+
+
+
+//STUNG!
+
+function beeSti(w)
+{
+    death = 8.67 * w;
+
+    return death;
+}
+
+alert("Have you ever wondered how many bee stings you could withstand before you died? Me neither, but we're going to find out.");
+beeSti(prompt("How many pounds do you weigh?"));
+
+alert("You could withstand " + death + " stings before you died.");
+console.log("You could withstand " + death + " stings before you died.");
