1ab06d137a282dcf39f4a4634ba03a401bc57e5f
jstanbridge <jstanbridge@fullsail.edu>
2014-02-27 11:03:11 -0500

    Added main function to Personal.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -2,4 +2,12 @@
 //26 Feb 2014
 //Functions Personal
 
-//This project will
+//This project will determine how many points a food costs in the Weight Watchers Points Plus system.
+
+alert("Hello! With a few simple bits of information, we can find out how many Weight Watchers points your food is worth.");
+//Introductory alert letting the user know what we're going to do.
+
+function wwPoints(pro, carb, fat, fiber){
+    result = (pro/11) + (carb/9) + (fat/4) - (fiber/35);
+    return result
+}
