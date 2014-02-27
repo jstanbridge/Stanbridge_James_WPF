5e30f2f55cf0872b5c95efc496b776cfbc316ea4
jstanbridge <jstanbridge@fullsail.edu>
2014-02-26 20:31:48 -0500

    Added comments to Stung.

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -29,14 +29,20 @@ console.log("The Circumference of the circle is " + circCal);
 //STUNG!
 
 function beeSti(w)
+//Here we are declaring the function, naming it, and setting it's parameters.
 {
     death = 8.67 * w;
+    //This line of code set up the calculation performed by the function when it is called.
 
     return death;
+    //This line returns the result of the above calculation to where the function was called from.
 }
 
 alert("Have you ever wondered how many bee stings you could withstand before you died? Me neither, but we're going to find out.");
+//Initial alert telling the user what we're doing.
 beeSti(prompt("How many pounds do you weigh?"));
+//This is the call for the function, and instead of a static parameter, we've set it up so it will dynamically add a value based on user input.
 
 alert("You could withstand " + death + " stings before you died.");
 console.log("You could withstand " + death + " stings before you died.");
+//These two lines return the result to the user and the console.
