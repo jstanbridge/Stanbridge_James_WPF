bc6b41c629be6c29afb8dd7a46ec410590988a2b
jstanbridge <jstanbridge@fullsail.edu>
2014-02-27 23:15:23 -0500

    Ready for commentary.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -5,30 +5,33 @@
 
 //This project will give users a super hero catch phrase based on inputs.
 
+alert("We're going to determine what your super hero catchphrase is based on some simple questions");
+
 var phrase;
 
 quiz(prompt("How many letters are in your last name?"), prompt("What is the last number of the year you were born?"), prompt("Choose one of the following: (1) Science (2) Mutation (3) Neither"));
 
 function quiz(lastName, yrBorn, mChoice){
     score = (+lastName) + (+yrBorn) + (+mChoice);
-    //score = +score;
     return score;
 }
 
 
 if (score < 15){
-    phrase = "Squirrel!";
+    phrase = "\"Squirrel!\"";
 }else if (score >= 15 && score < 20){
-    phrase = "Here I come to save the day!";
+    phrase = "\"Here I come to save the day!\"";
 }else if (score >= 20 && score < 25){
-    phrase = "It's clobberin' time!";
+    phrase = "\"It's clobberin' time!\"";
 }else if (score >= 25 && score < 30){
-    phrase = "Cowabunga!";
+    phrase = "\"Cowabunga!\"";
 }else if (score > 30){
-    phrase = "Danger, Will Robinson!";
+    phrase = "\"Danger, Will Robinson!\"";
 }
 
-
+alert("Your super hero catchphrase is " + phrase + ".");
 console.log(score);
 console.log(phrase);
+
+
 
