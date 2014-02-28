af7eda3c21ed17708d82a2395367e4a6aecd001e
jstanbridge <jstanbridge@fullsail.edu>
2014-02-27 22:06:12 -0500

    Setup framework for Wacky.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -2,4 +2,28 @@
 //26 Feb 2014
 //Functions Wacky
 
-//This project will
+//This project will give users a super hero catch phrase based on inputs.
+
+/*
+It's clobberin' time! (<25 && >=20)
+Danger! Will Robinson! (>30)
+To infinity and beyond! (YES/NO)
+Squirrel! (<15)
+Cowabunga! (<=30 && >=25)
+Here I come to save the day! (<20 && >= 15)
+ */
+
+/*
+How many letters are in your last name?
+What is the last number of the year you were born?
+Choose one of the following: (1) Science (2) Mutation (3) Neither
+Do you like outer space? (YES/NO)
+ */
+
+/*
+var score
+ */
+
+/*
+var if 
+ */
