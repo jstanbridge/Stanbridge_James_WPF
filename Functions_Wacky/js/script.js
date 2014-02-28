452db421e96581761e2f069bb7356998e51b637c
jstanbridge <jstanbridge@fullsail.edu>
2014-02-27 22:17:41 -0500

    Added ternary to Wacky.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -12,11 +12,12 @@ Squirrel! (<15)
 Cowabunga! (<=30 && >=25)
 Here I come to save the day! (<20 && >= 15)
  */
-
+var phrase;
 
 quiz(prompt("How many letters are in your last name?"), prompt("What is the last number of the year you were born?"), prompt("Choose one of the following: (1) Science (2) Mutation (3) Neither"));
 var space = prompt("Do you like outer space? (YES/NO)");
 
+space = true ? (phrase = "It's clobberin' time!") : (phrase = score);
 
 /*
 var score
