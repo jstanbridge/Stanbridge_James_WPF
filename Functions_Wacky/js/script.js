4181445956ab0bcdc0afa1efee91e4d0004a130e
jstanbridge <jstanbridge@fullsail.edu>
2014-02-27 22:27:18 -0500

    Added phrases to wacky.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -17,12 +17,24 @@ var phrase;
 quiz(prompt("How many letters are in your last name?"), prompt("What is the last number of the year you were born?"), prompt("Choose one of the following: (1) Science (2) Mutation (3) Neither"));
 var space = prompt("Do you like outer space? (YES/NO)");
 
-space = true ? (phrase = "It's clobberin' time!") : (phrase = score);
+space = true ? (phrase = "To infinity and beyond!") : (phrase = score);
 
 function quiz(lastName, yrBorn, mChoice){
     score = lastName + yrBorn + mChoice;
     return score
 }
+
+if (score <15){
+    phrase = "Squirrel!";
+}else if (score >= 15 && score < 20){
+    phrase = "Here I come to save the day!";
+}else if (score >= 20 && score < 25){
+    phrase = "It's clobberin' time!";
+}else if (score >= 25 && score < 30){
+    phrase = "Cowabunga!";
+}else phrase = "Danger, Will Robinson!";
+
+
 
 /*
 var score
