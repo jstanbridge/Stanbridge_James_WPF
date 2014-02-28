9226e3cd01f3ce92d20deeeaa29d5b3dcf865253
jstanbridge <jstanbridge@fullsail.edu>
2014-02-27 20:53:43 -0500

    Added final comments to Industry.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -18,13 +18,21 @@ function wages(pay, hrsWrk, otHrs){
 
 
 wages(prompt("How much do you make per hour?"), prompt("How many hours did you work this week?"), prompt("How many hours of overtime did you work this week?"));
+//This is the call for the function above. Parameters are set using prompts for user input.
 
-if (paycheck > 833){
+var natAvg = 833;
+//This variable holds the US weekly nation average income.
+
+if (paycheck > natAvg){
+    //Here we are setting up a condition for a weekly income that is above the national average.
     alert("Congratulations, with the $" + paycheck + " you made this week, you've exceeded the US national average for hourly paid jobs.");
     console.log("Congratulations, with the $" + paycheck + " you made this week, you've exceeded the US national average for hourly paid jobs.");
+    //If the condition above is met, then the result prints to both the user and the console log.
 }else if (paycheck === 833){
+    //Here we are setting up a condition for a weekly income that is equal to the national average.
     alert("With the $" + paycheck + " you made this week, you tied the US national average for hourly paid jobs.");
     console.log("With the $" + paycheck + " you made this week, you tied the US national average for hourly paid jobs.");
+    //If the condition above is met, then the result prints to both the user and the console log.
 }else alert("Unfortunately, the $" + paycheck + " that you made this week does not exceed the US national average for hourly paid jobs.");
 console.log("Unfortunately, the $" + paycheck + " that you made this week does not exceed the US national average for hourly paid jobs.");
-
+//Lastly, if none of the other conditions are met, then we have a result of < the national average and the result prints to both the user and the console log.
